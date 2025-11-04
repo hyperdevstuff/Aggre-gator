15afaec46c35b92f6e91cca40b56af2e2fe8f2f3
Harsh Chauhan <168334425+hyperdevstuff@users.noreply.github.com>
2025-11-05 00:05:15 +0530

    refactor: better env var loading

--- app/src/utils/auth.ts
+++ app/src/utils/auth.ts
@@ -7,11 +7,15 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "pg", schema }),
   baseURL: (process.env.BETTER_AUTH_URL as string) || "http://localhost:3000",
   emailAndPassword: { enabled: true },
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-  },
+  ...(process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET
+      ? {
+          socialProviders: {
+            google: {
+              clientId: process.env.GOOGLE_CLIENT_ID,
+              clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            },
+          },
+        }
+      : {}),
   session: { expiresIn: 60 * 60 * 24 * 7 }, // 7 days
 });
