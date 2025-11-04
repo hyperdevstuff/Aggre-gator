87af1bcfaa02039565379311d11a6464c0f29d56
Harsh Chauhan <168334425+hyperdevstuff@users.noreply.github.com>
2025-11-04 21:19:35 +0530

    api: add google oauth provider(better-auth)

--- app/src/utils/auth.ts
+++ app/src/utils/auth.ts
@@ -5,7 +5,13 @@ import * as schema from "../db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "pg", schema }),
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: (process.env.BETTER_AUTH_URL as string) || "http://localhost:3000",
   emailAndPassword: { enabled: true },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   session: { expiresIn: 60 * 60 * 24 * 7 }, // 7 days
 });
