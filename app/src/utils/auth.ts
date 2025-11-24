bffa59129124f1f1eee557bbfe657df9b97247dc
Harsh Chauhan <168334425+hyperdevstuff@users.noreply.github.com>
2025-11-25 00:26:08 +0530

    refactor: removed hardcoded origins and better SSO google auth

--- app/src/utils/auth.ts
+++ app/src/utils/auth.ts
@@ -5,12 +5,13 @@ import * as schema from "../db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "pg", schema }),
-  baseURL: (process.env.BETTER_AUTH_URL as string) || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL as string,
   emailAndPassword: { enabled: true },
   ...(process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET
     ? {
         socialProviders: {
           google: {
+            prompt: "select_account",
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
           },
