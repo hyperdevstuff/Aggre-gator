65a47a9f0a965accb8fa1fbad1991caa0250266d
Harsh Chauhan <168334425+hyperdevstuff@users.noreply.github.com>
2025-11-26 16:49:07 +0530

    refactor: better sidebar

--- client/src/routes/_protected.tsx
+++ client/src/routes/_protected.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import { authClient } from "@/lib/auth-client";
 import { Toaster } from "@/components/ui/sonner";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export const Route = createFileRoute("/_protected")({
   beforeLoad: async ({ location }) => {
@@ -21,8 +24,17 @@ export const Route = createFileRoute("/_protected")({
 function ProtectedLayout() {
   return (
     <>
-      <Outlet />
-      <Toaster />
+      <SidebarProvider defaultOpen={!useIsMobile()}>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger hidden={!useIsMobile()} />
+          {/*I don't need sidebar toggle for lg md only mobile which takes over entire screen*/}
+          {/*TODO: fix trigger for responsive layouto*/}
+          {/*Fix: shadcn useSidebar hook*/}
+          <Outlet />
+          <Toaster />
+        </main>
+      </SidebarProvider>
     </>
   );
 }
